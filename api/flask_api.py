88cf93b3b4bf14c9d0e6bd2990c9661fb163031d
BernardWong97 <ben.wc88@gmail.com>
2021-02-26 09:51:42 +0800

    Added components

--- api/flask_api.py
+++ api/flask_api.py
@@ -1,7 +1,36 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'examples'
+mysql.init_app(app)
+con = mysql.connect()
 
 @app.route('/login')
 def login():
-    return {'token': 'test123'}
+    cur = con.cursor()
+    cur.execute("select * from users")
+    con.commit()
+    table = []
+    
+    for id, fname, lname, uname, pw, created_date, modified_date, is_online in cur.fetchall():
+        row = dict()
+        row['id'] = id
+        row['first_name'] = fname
+        row['last_name'] = lname
+        row['user_name'] = uname
+        row['password'] = pw
+        row['create_date'] = created_date
+        row['modified_date'] = modified_date
+        row['is_online'] = is_online
+        table.append(row)
+
+    return jsonify(table)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, {useState} from 'react'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {AppContext} from './libs/contextLib'
 
 import Dashboard from './components/Dashboard/Dashboard'
 import Preferences from './components/Preferences/Preferences'
-import Login from './components/Login/Login';
+import NotFound from './components/Pages/NotFound'
+import Login from './components/Pages/Login';
 
 function App() {
-  const [token, setToken] = useState()
-
-  if(!token) {
-    return <Login setToken={setToken}/>
-  }
+  const [isAuthenticated, userHasAuthenticated] = useState(false)
 
   return (
     <div className="App">
       <h1>Application</h1>
+      <
       <BrowserRouter>
         <Switch>
+          <Route path="/login">
+            <Login/>
+          </Route>
           <Route path="/dashboard">
             <Dashboard/>
           </Route>
           <Route path="/preferences">
             <Preferences/>
+          </Route>
+          <Route path=''>
+            <NotFound/>
           </Route>
         </Switch>
       </BrowserRouter>
